1a72f7ab79eab7030aba36b3643227234163cea4
Darshit Bhuva <darshitbhuva9330@gmail.com>
2024-10-11 17:43:31 +0530

    playlist and comment controller added

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,31 +1,84 @@
 import mongoose from "mongoose"
-import {Comment} from "../models/comment.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { Comment } from "../models/comment.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
-    //TODO: get all comments for a video
-    const {videoId} = req.params
-    const {page = 1, limit = 10} = req.query
+    
+    const { videoId } = req.params
+    const { page = 1, limit = 10 } = req.query
+
+    const pageNumber = Number(page);
+    const limitNumber = Number(limit);
+    const skipNumber = (pageNumber - 1) * limitNumber;
+
+    const comments = await Comment.aggregate([
+        {
+            $match: { video: new mongoose.Types.ObjectId(videoId) }
+        },
+        {
+            $skip: skipNumber
+        },
+        {
+            $limit: limitNumber
+        }
+    ])
+
+    return res.status(200).json(new ApiResponse(200, comments, "Comments fetched successfully"));
 
 })
 
 const addComment = asyncHandler(async (req, res) => {
-    // TODO: add a comment to a video
+    const { content } = req.body;
+    const { videoId } = req.params;
+
+    const comment = await Comment.create({
+        content,
+        video: videoId,
+        owner: req.user._id
+    });
+
+    return res.status(201).json(new ApiResponse(201, comment, "New Comment Added Successfully"));
+
 })
 
 const updateComment = asyncHandler(async (req, res) => {
-    // TODO: update a comment
+
+    const { commentId } = req.params;
+    const { content } = req.body;
+
+    const comment = await Comment.findById(commentId);
+
+    if (!comment)
+        throw new ApiError(404, "Comment is not found");
+
+    const updatedComment = await Comment.findByIdAndUpdate(
+        commentId,
+        {
+            $set: {
+                content
+            }
+        },
+        { new: true });
+
+    return res.status(200).json(new ApiResponse(200, updatedComment, "Comment Updated Successfully"))
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
-    // TODO: delete a comment
+    const { commentId } = req.params;
+
+    const deletedComment = await Comment.findByIdAndDelete(commentId);
+
+    if (!deletedComment)
+        throw new ApiError(404, "Comment is not found");
+
+    return res.status(200).json(new ApiResponse(200, deletedComment, "Comment Deleted Successfully"));
 })
 
 export {
-    getVideoComments, 
-    addComment, 
+    getVideoComments,
+    addComment,
     updateComment,
-     deleteComment
-    }
+    deleteComment
+}
--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,44 +1,94 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Playlist} from "../models/playlist.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose, { isValidObjectId } from "mongoose"
+import { Playlist } from "../models/playlist.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
 
 const createPlaylist = asyncHandler(async (req, res) => {
-    const {name, description} = req.body
+    const { name, description } = req.body
 
-    //TODO: create playlist
+    const playlist = await Playlist.create({
+        name,
+        description,
+        owner: req.user._id
+    })
+
+    return res.status(201).json(new ApiResponse(201, playlist, "New Playlist created"))
 })
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
-    const {userId} = req.params
-    //TODO: get user playlists
+    const { userId } = req.params
+
+    const playlist = await Playlist.find({ owner: userId });
+
+    if (!playlist || playlist.length === 0)
+        throw new ApiError(404, "User does not have any playlists")
+
+    return res.status(200).json(new ApiResponse(200, playlist, "All available playlist fetched"));
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    //TODO: get playlist by id
+    const { playlistId } = req.params
+
+    const playlist = await Playlist.findById(playlistId);
+
+    if (!playlist) {
+        throw new ApiError(404, "Requested playlist is not found");
+    }
+
+    return res.status(200).json(new ApiResponse(200, playlist, "Playlist fetched successfully"));
 })
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
-    const {playlistId, videoId} = req.params
+    const { playlistId, videoId } = req.params
+
+    const playlist = await Playlist.findById(playlistId);
+
+    if (!playlist) {
+        throw new ApiError(404, "Requested playlist is not found");
+    }
+
+    playlist.videos.push(videoId);
+    await playlist.save();
+
+    return res.status(200).json(new ApiResponse(200, playlist, "Video added to playlist"));
 })
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
-    const {playlistId, videoId} = req.params
-    // TODO: remove video from playlist
+    const { playlistId, videoId } = req.params
+    
+    const playlist = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $pull : {videos : new mongoose.Types.ObjectId(videoId)}
+        },
+        {new : true}
+    );
+
+    if(!playlist)
+    {
+        throw new ApiError(404, "Playlist is not found");
+    }
+
+    return res.status(200).json(new ApiResponse(200, playlist, "Video removed from playlist successfully"));
 
 })
 
 const deletePlaylist = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    // TODO: delete playlist
+    const { playlistId } = req.params
+    
+    const deletedPlaylist = await Playlist.findByIdAndDelete(playlistId);
+
+    if(!deletedPlaylist)
+        throw new ApiError(404, "Playlist not found");
+
+    return res.status(200).json(new ApiResponse(200, deletedPlaylist, "Playlist deleted Successfully"));
 })
 
 const updatePlaylist = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    const {name, description} = req.body
+    const { playlistId } = req.params
+    const { name, description } = req.body
     //TODO: update playlist
 })
 
